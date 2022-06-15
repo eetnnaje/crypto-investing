93e60f8b680e2d1de2a99bcee2715de638183fd7
ejelome <ejelome@gmail.com>
2022-06-15 16:55:46 +0200

    feat(home): add fiat and crypto calculations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
 import { PriceService } from '../services/price.service';
 import { Price } from '../types/price.type';
+
+export type Token = {
+  [key: string]: number;
+};
+
+export type Tokens = {
+  [key: string]: Token;
+};
+
+const TOKEN = { price: 0, amount: 0, value: 0 };
 
 @Component({
   selector: 'app-home',
@@ -9,24 +18,56 @@ import { Price } from '../types/price.type';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  public investment: number = 0;
+  public extras: number = 0;
+  public token: Tokens = {
+    busd: TOKEN,
+    btc: TOKEN,
+    eth: TOKEN,
+    axs: TOKEN,
+  };
   public prices: Price[] = [];
 
-  constructor(private readonly _priceService: PriceService) {}
+  constructor(private priceService: PriceService) {}
 
   ngOnInit(): void {
-    this._priceService.getPrices().subscribe((prices) => {
+    this.priceService.getPrices().subscribe((prices) => {
       this.prices = prices;
     });
 
     const minute = 60 * 1_000;
     setInterval(() => {
-      this._priceService.getPrices().subscribe((prices) => {
+      this.priceService.getPrices().subscribe((prices) => {
         this.prices = prices;
       });
     }, minute);
   }
 
-  getPrice(symbol: string): Price | undefined {
-    return this.prices.find(({ symbol: s }) => symbol === s);
+  handleInvestment(amount: number): void {
+    this.investment = amount;
+  }
+
+  handleExtras(amount: number): void {
+    this.extras = amount;
+  }
+
+  getPrice(symbol: string): number {
+    const price = this.prices.find(({ symbol: s }) => symbol === s);
+    if (price) return +price.price;
+    return 0;
+  }
+
+  handleTokenValue(token: string, amount: number): void {
+    const symbol = [token.toUpperCase(), 'BUSD'].join('');
+    const price = this.getPrice(symbol);
+    const value = amount * price;
+    this.token[token] = { amount, price, value };
+  }
+
+  handleStableCoinValue(stableCoin: string, amount: number): void {
+    const symbol = stableCoin.toLowerCase();
+    const price = amount;
+    const value = amount;
+    this.token[symbol] = { amount, price, value };
   }
 }
